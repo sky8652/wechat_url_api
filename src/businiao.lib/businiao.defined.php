03e4992610dd1832d6b53edbf88faa190c02f018
William King <magallania@gmail.com>
2020-03-31 18:37:21 +0800

    ver:1.1.7

--- src/businiao.lib/businiao.defined.php
+++ src/businiao.lib/businiao.defined.php
@@ -3,7 +3,7 @@
  * BUSINIAO_API_HOST https://wechaturl.us/api/ 
  * BUSINIAO_API_HOST https://v94.cn/api/ 这个适合中国大陆的服务器访问
  */
-define('BUSINIAO_API_VERSION','1.1.6');
+define('BUSINIAO_API_VERSION','1.1.7');
 define('BUSINIAO_API_HOST','https://www-test.wechaturl.us/api/');
 define('BUSINIAO_API_TYPE_DomainShortUrl','DomainShortUrl.json');
 define('BUSINIAO_API_TYPE_GetWechatShortUrl','GetWechatShortUrl.json');
