bf5d4f5cf127db17c03c2e67160d8730c8d65f69
William King <magallania@gmail.com>
2020-03-31 19:00:43 +0800

    edit api url

--- src/businiao.lib/businiao.defined.php
+++ src/businiao.lib/businiao.defined.php
@@ -4,7 +4,7 @@
  * BUSINIAO_API_HOST https://v94.cn/api/ 这个适合中国大陆的服务器访问
  */
 define('BUSINIAO_API_VERSION','1.1.7');
-define('BUSINIAO_API_HOST','https://www-test.wechaturl.us/api/');
+define('BUSINIAO_API_HOST','https://wechaturl.us/api/');
 define('BUSINIAO_API_TYPE_DomainShortUrl','DomainShortUrl.json');
 define('BUSINIAO_API_TYPE_GetWechatShortUrl','GetWechatShortUrl.json');
 define('BUSINIAO_API_TYPE_GetWeiboShortUrl','GetWeiboShortUrl.json');
